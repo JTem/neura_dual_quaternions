9a06a748dcad5f7028880347b9f21e5322f2aa4c
JTem <temminghoff.j@web.de>
2023-12-09 21:48:03 +0100

    changed version to 0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='neura_dual_quaternions',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     install_requires=[
         'numpy'
